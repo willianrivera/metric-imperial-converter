24220acaffb2f5dcb898aec7fee44e2b26d4d601
nibble0101 <mj.mawa@yahoo.com>
2021-10-05 22:00:31 +0300

    Fix incorrect return string

--- routes/api.js
+++ routes/api.js
@@ -13,19 +13,25 @@ module.exports = function (app) {
     }
     const initNum = convertHandler.getNum(input);
     const initUnit = convertHandler.getUnit(input);
+
+    if (initNum === "invalid number" && initUnit === "invalid unit") {
+      return res.send("invalid number and unit");
+    }
     if (initNum === "invalid number") {
       return res.send("invalid number");
     }
-    if( initUnit === "invalid unit"){
+    if (initUnit === "invalid unit") {
       return res.send("invalid unit");
     }
-    if( initNum === "invalid number" && initUnit === "invalid unit"){
-      return res.send("invalid unit and number");
-    }
-    
+
     const returnUnit = convertHandler.getReturnUnit(initUnit);
     const returnNum = convertHandler.convert(initNum, initUnit);
-    const string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
-    res.json({ initNum, initUnit, returnNum, returnUnit, string});
+    const string = convertHandler.getString(
+      initNum,
+      initUnit,
+      returnNum,
+      returnUnit
+    );
+    res.json({ initNum, initUnit, returnNum, returnUnit, string });
   });
 };
