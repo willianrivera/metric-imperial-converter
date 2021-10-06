e4fc15ed97407484c6b98f0e5bc8785323a2141f
nibble0101 <mj.mawa@yahoo.com>
2021-10-06 23:07:51 +0300

    Modify tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,32 +5,42 @@ const ConvertHandler = require("../controllers/convertHandler.js");
 let convertHandler = new ConvertHandler();
 
 suite("Unit Tests", function () {
-  test("Testing input number", () => {
+  test("Testing valid whole number input", () => {
     assert.strictEqual(
       convertHandler.getNum("2kg"),
       2,
-      "correctly read valid whole number input"
+      "Correctly read valid whole number input"
     );
+  });
+  test("Testing valid decimal input", () => {
     assert.strictEqual(
       convertHandler.getNum("2.5lbs"),
       2.5,
-      "correctly read valid decimal input"
+      "Correctly read valid decimal input"
     );
+  });
+  test("Testing valid fractional input", () => {
     assert.strictEqual(
       convertHandler.getNum("1/5kg"),
       0.2,
-      "correctly read valid fractional input"
+      "Correctly read valid fractional input"
     );
+  });
+  test("Testing valid fractional input with decimal", () => {
     assert.strictEqual(
       convertHandler.getNum("0.2/0.5kg"),
       0.4,
-      "correctly read valid fractional input with decimal"
+      "Correctly read valid fractional input with decimal"
     );
+  });
+  test("Testing invalid double fraction input", () => {
     assert.strictEqual(
       convertHandler.getNum("2/2/7kg"),
       "invalid number",
-      "return error for invalid double fraction input"
+      "Return error for invalid double fraction input"
     );
+  });
+  test("Testing no numeric input", () => {
     assert.strictEqual(
       convertHandler.getNum("lbs"),
       1,
@@ -64,6 +74,14 @@ suite("Unit Tests", function () {
       convertHandler.getUnit("2kg"),
       "kg",
       "correctly read kg"
+    );
+  });
+
+  test("Testing invalid input unit", () => {
+    assert.strictEqual(
+      convertHandler.getReturnUnit("invalidUnit"),
+      "invalid unit",
+      "Correctly return error message for invalid input unit"
     );
   });
 
@@ -133,36 +151,50 @@ suite("Unit Tests", function () {
     );
   });
 
-  test("Correctly make conversions", () => {
+  test("Converting gal to L", () => {
     assert.strictEqual(
       convertHandler.convert(2, "gal"),
       7.57082,
       "Correctly convert 2gal to 7.57082L"
     );
+  });
+  test("Converting L to gal", () => {
     assert.strictEqual(
       convertHandler.convert(2, "L"),
       0.52834,
       "Correctly convert 2L to 0.52834gal"
     );
+  });
+  test("Converting mi to km", () => {
     assert.strictEqual(
       convertHandler.convert(2, "mi"),
       3.21868,
       "Correctly convert 2mi to 3.21868km"
     );
+  });
+  test("Converting km to mi", () => {
     assert.strictEqual(
       convertHandler.convert(2, "km"),
       1.24275,
       "Correctly convert 2km to 1.24275mi"
     );
+  });
+  test("Converting lbs to kg", () => {
     assert.strictEqual(
       convertHandler.convert(2, "lbs"),
       0.90718,
       "Correctly convert 2lbs to 0.90718kg"
     );
+  });
+  test("Converting kg to lbs", () => {
     assert.strictEqual(
       convertHandler.convert(2, "kg"),
       4.40925,
       "Correctly convert 2kg to 4.40925lbs"
     );
   });
+
+  // test("Correctly make conversions", () => {
+
+  // });
 });
